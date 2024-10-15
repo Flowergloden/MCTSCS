53b61d76e4753668f6c0c4d5574ed4f72c3340c1
FlowerGolden <1327097302@qq.com>
2024-10-15 15:47:55 +0800

    feat: tree property def

--- MonteCarloTree.cs
+++ MonteCarloTree.cs
@@ -1,7 +1,16 @@
 ﻿namespace MonteCarloTreeSearch
 {
-    public class MonteCarloTree
+    public class MonteCarloTree<T, TGame> where TGame : IGame<T>
     {
-        
+        private MonteCarloTreeNode _root;
+        private readonly float _c;
+        private TGame _game;
+
+        public MonteCarloTree(float c, TGame game)
+        {
+            _c = c;
+            _game = game;
+            _root = new MonteCarloTreeNode(0, 0, _c);
+        }
     }
 }
