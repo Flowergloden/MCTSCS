07fc7a11839a4fa5b3d147447ff8c96387a5523f
FlowerGolden <1327097302@qq.com>
2024-10-15 15:03:19 +0800

    feat: tree node def

---
+++ Math.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonteCarloTreeSearch
+{
+    public static class Math
+    {
+        public static float UCT(float t, float n, float c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MonteCarloTreeNode.cs
+++ MonteCarloTreeNode.cs
@@ -2,6 +2,61 @@
 {
     public class MonteCarloTreeNode
     {
-        
+        private float _t;
+        private float _n;
+        private float _c;
+        private bool _hasChanged = true;
+        private float _s;
+
+        public MonteCarloTreeNode(float t, float n, float c)
+        {
+            _t = t;
+            _n = n;
+            _c = c;
+        }
+
+        public float T
+        {
+            get => _t;
+            set
+            {
+                _t = value;
+                _hasChanged = true;
+            }
+        }
+
+        public float N
+        {
+            get => _n;
+            set
+            {
+                _n = value;
+                _hasChanged = true;
+            }
+        }
+
+        public float C
+        {
+            get => _c;
+            set
+            {
+                _c = value;
+                _hasChanged = true;
+            }
+        }
+
+        public float S
+        {
+            get
+            {
+                if (_hasChanged)
+                {
+                    _s = Math.UCT(_t, _n, _c);
+                    _hasChanged = false;
+                }
+
+                return _s;
+            }
+        }
     }
 }
