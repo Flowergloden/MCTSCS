3f2602810371c26d349222886288f6c9500145a3
FlowerGolden <1327097302@qq.com>
2024-10-15 20:20:32 +0800

    feat: impl UCT

--- Math.cs
+++ Math.cs
@@ -4,9 +4,15 @@ namespace MonteCarloTreeSearch
 {
     public static class Math
     {
-        public static float UCT(float t, float n, float c)
+        public static float UCT(float t, float n, float N, float c)
         {
-            throw new NotImplementedException();
+            // TODO: use quick natural log & sqrt
+            if (n == 0)
+            {
+                return float.MaxValue;
+            }
+
+            return t + c * MathF.Sqrt(MathF.Log(N) / n);
         }
     }
 }
